6164fddff57deeddcaf4e627c9c2706ef1cf8702
hufeng <hufeng990721@163.com>
2023-07-25 14:55:21 +0800

    配置@vanilla-extract

--- modern.config.ts
+++ modern.config.ts
@@ -1,5 +1,5 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
-
+import { VanillaExtractPlugin } from '@vanilla-extract/webpack-plugin';
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
   runtime: {
@@ -9,4 +9,24 @@ export default defineConfig({
     port: 1010,
   },
   plugins: [appTools()],
+  tools: {
+    terser: {
+      terserOptions: {
+        ecma: 2016,
+        module: true,
+        compress: {
+          passes: 2,
+          keep_infinity: true,
+          arguments: true,
+          ecma: 2016,
+        },
+        mangle: {
+          safari10: false, // drop safari10/11 support
+        },
+      },
+    },
+    webpackChain: chain => {
+      chain.plugin('vanilla-extract').use(VanillaExtractPlugin);
+    },
+  },
 });
